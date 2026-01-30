7a00d35efbbb6a181e3ab73b7502f7698ec5801a
y0ngha <dev.y0ngha@gmail.com>
2026-01-31 04:10:23 +0900

    Solved Leetcode Problem No.217

---
+++ leetcode/217.ts
@@ -0,0 +1,27 @@
+/**
+ * @Title: 217. Contains Duplicate
+ * @Link: https://leetcode.com/problems/contains-duplicate/description/
+ * @Solved: Jan 28, 2026 03:07
+ */
+
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    const n = nums.length;
+    k %= n;
+
+    const reverse = (start: number, end: number) => {
+        while (start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]]; // Swap
+            start++;
+            end--;
+        }
+    }
+
+    reverse(0, n - 1);
+    reverse(0, k - 1);
+    reverse(k, n - 1);
+}
+
+console.log(rotate([1,2,3,4,5,6,7], 3))
